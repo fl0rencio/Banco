e8cc94e881326a0aa5e468aa385604630a0d56bf
Wendel Florencio <wendel.github@gmail.com>
2022-11-23 01:44:46 -0300

    .

---
+++ Banco/Cliente.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+
+namespace Banco
+{
+    public class Cliente
+    {
+
+        public int Conta { get; private set; }
+
+        public double Saldo { get; private set; }
+
+        public string Titular { get; set; }
+
+        public Cliente(int conta, string titular)
+        {
+            Conta = conta;
+            Titular = titular;
+        }
+
+        
+
+        public Cliente(double saldoInicial, int conta, string titular) : this(conta, titular)
+        {
+            Depositar (saldoInicial);
+        }
+
+
+        public void Depositar(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Sacar(double quantia)
+        {
+            Saldo -= quantia + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return
+
+                "CONTA: " + Conta +
+               " , " + " TITULAR: " + Titular +
+               " , " + "SALDO: R$ " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Banco/Program.cs
@@ -0,0 +1,56 @@
+﻿using Banco;
+using System.Globalization;
+
+Cliente cliente;
+
+Console.Write("ENTRE COM O NUMERO DA CONTA: ");
+int conta = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.Write("DIGITE O NOME DO TITULAR DA CONTA: ");
+string titular = Console.ReadLine();
+Console.WriteLine();
+
+Console.Write("HAVERA DEPOSITO INICIAL? (s / n) ");
+char simOuNao = char.Parse(Console.ReadLine());
+
+if (simOuNao == 's' || simOuNao == 'S')
+{
+    Console.WriteLine();
+    Console.Write("Digite o valor do deposito incial: ");
+    double saldoInicial = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+    cliente = new Cliente(saldoInicial,conta,titular);
+    
+
+    
+}
+else
+{
+     cliente = new Cliente(conta, titular);
+    
+}
+
+Console.WriteLine();
+Console.WriteLine("DADOS DA CONTA: ");
+Console.WriteLine(cliente);
+
+Console.WriteLine();
+Console.Write("Qual valor para deposito? ");
+double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+cliente.Depositar(quantia);
+Console.WriteLine();
+Console.Write("DADOS DA CONTA ATUALIZADOS : ");
+Console.WriteLine(cliente);
+
+Console.WriteLine();
+Console.Write("Digite o valor desejado para saque: ");
+quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+cliente.Sacar(quantia);
+Console.WriteLine();
+Console.Write("DADOS DA CONTA ATUALIZADOS : ");
+Console.WriteLine(cliente);
+
+
+
+
+
+
